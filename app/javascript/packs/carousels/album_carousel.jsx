f061272f7c4e7a9e515d6bb9af9df48c1a458ffd
Eric Oligney <EOengineer@gmail.com>
2017-09-21 20:06:12 -0400

    added responsive settings to carousel component

--- app/javascript/packs/carousels/album_carousel.jsx
+++ app/javascript/packs/carousels/album_carousel.jsx
@@ -16,16 +16,70 @@ class AlbumCarousel extends React.Component {
     })
   }
 
+  buildResponsiveSettings = () => {
+
+    let settings = [
+      {
+        // fullhd
+        breakpoint: 3000,
+        settings: {
+          slidesToShow: 5,
+          dots: true
+        }
+      },
+      {
+        // widescreen
+        breakpoint: 1392,
+        settings: {
+          slidesToShow: 4,
+          dots: true
+        }
+      },
+      {
+        // desktop
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          dots: true
+        }
+      },
+      {
+        // tablet
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 3,
+          dots: true
+        }
+      },
+      {
+        //most phones
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        //most phones
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+
+    return settings;
+  }
+
   componentDidMount() {
     let slider         = this.refs.slider;
     let carouselConfig = {
       infinite: false,
-      slidesToShow: 3,
       slidesToScroll: 1,
       arrows: true,
-      dots: true
+      dots: true,
+      adaptiveWidth: true,
+      responsive: this.buildResponsiveSettings()
     };
-
 
 
     $(slider).slick(carouselConfig)
@@ -36,7 +90,7 @@ class AlbumCarousel extends React.Component {
     let carouselTitle = this.props.carouselTitle
 
     return (
-      <section className="section">
+      <section className="section is-bold is-light">
         <div className="container">
           <h1 className="title">{carouselTitle}</h1>
 
